f9f0ca420ba422956b4f31f0dd489278aac9dd74
Nicole Norman <nnorman312@gmail.com>
2020-05-18 10:51:23 -0500

    Update

--- Plot.ly/static/js/app.js
+++ Plot.ly/static/js/app.js
@@ -39,7 +39,7 @@ function updateMetadata(sample) {
       marker: {size: 28, color:'850000'},
       value: result.wfreq,
       title: 'Belly Button Washing Frequency<br> Scrubs per Week',
-      titlefont: {family: "Georgia", "sans-serif"},
+      titlefont: {family: '"Palatino Linotype", "Book Antiqua", Palatino, serif'},
       type: "indicator",
       mode: "gauge+number"
     }
@@ -53,9 +53,8 @@ function updateMetadata(sample) {
      line: {
      color: '600000'
      },
-     paper_bgcolor='rgba(0,0,0,0)',
-     plot_bgcolor='rgba(0,0,0,0)'
-     font: { color: "##FFB6C1", family: "Georgia", "sans-serif"}
+     paper_bgcolor: "#a5bdc6",
+     font: { color: "#85541d", family: "Serif" }
    };
 
   
